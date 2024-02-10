49e09ae1669b6d18c0d0746ae72841363db68da0
PharaoOoh <125400541+PharaoOoh@users.noreply.github.com>
2024-02-11 01:23:18 +0200

    Add files via upload

---
+++ To-Do-App/precache-manifest.77907c0eccac2dd7b9a3fdc501d2dfe4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3eeece384655a3f3595a",
+    "url": "./static/css/main.2a18fdec.chunk.css"
+  },
+  {
+    "revision": "3eeece384655a3f3595a",
+    "url": "./static/js/main.3eeece38.chunk.js"
+  },
+  {
+    "revision": "2e8f1a6b9f28a667d85d",
+    "url": "./static/js/1.2e8f1a6b.chunk.js"
+  },
+  {
+    "revision": "b43983f2aca8864c7934",
+    "url": "./static/css/2.cd2179cb.chunk.css"
+  },
+  {
+    "revision": "b43983f2aca8864c7934",
+    "url": "./static/js/2.b43983f2.chunk.js"
+  },
+  {
+    "revision": "3c4ab64ea93c86f4b92b",
+    "url": "./static/js/runtime~main.3c4ab64e.js"
+  },
+  {
+    "revision": "76f5635dde1b195e33ed074feac8b889",
+    "url": "./index.html"
+  }
+];
